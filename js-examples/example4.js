564d4acf9bc4abe780b7ec48bb6a80250e1a21b2
hemeshhere <hh9199939655@gmail.com>
2026-01-21 15:57:31 +0530

    added examples

---
+++ js-examples/example4.js
@@ -0,0 +1,3 @@
+let x=20;
+let y=30;
+console.log(x==20 && y==30);
---
+++ js-examples/example5.js
@@ -0,0 +1,15 @@
+let x=20;
+switch (x) {
+    case 10:
+        console.log("value is 10");
+        break;
+    case 20:
+        console.log("value is 20");
+        break;
+    case 30:
+        console.log("value is 30");
+        break;
+    default:
+        console.log("value is none");
+        break;
+}
